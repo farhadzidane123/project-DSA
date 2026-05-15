203d7373540c18e337e3ae586db41d8f7c026424
Farhad Zidane <24001142@siswa.um.edu.my>
2026-05-15 16:53:56 +0800

    base for data structures

---
+++ src/datastructures/TriageManager.java
@@ -0,0 +1,24 @@
+package datastructures;
+
+import java.util.*;
+
+public class TriageManager {
+    public TriageManager() {
+    }
+
+    // Write the logic to check severity and put it into the right queue
+    public void addCallToQueue(EmergencyCall call) {
+    }
+
+    /*
+     * Write logic to check the priority queue first
+     * Pull from the standard queue if the priority one is empty
+     */
+    public EmergencyCall getNextHighestPriorityCall() {
+        return null; // Implement queue polling logic
+    }
+
+    public boolean hasWaitingCalls() {
+        return false; // Check if both queues are empty
+    }
+}
