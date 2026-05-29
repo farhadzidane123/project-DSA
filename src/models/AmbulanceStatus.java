29862f2de161d9bf01bd87a155e4729173668583
abuhafaisal12-afk <abuhafaisal12@gmail.com>
2026-05-29 14:55:47 +0800

    insert 4 ambulance status

--- src/models/AmbulanceStatus.java
+++ src/models/AmbulanceStatus.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
-
-/**
- *
- * @author Lenovo
- */
+package models;
 public enum AmbulanceStatus {
-    
+    AVAILABLE,
+    BUSY,
+    MAINTENANCE,
+    OFFLINE
 }
